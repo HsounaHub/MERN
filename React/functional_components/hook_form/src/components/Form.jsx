215da805f01be4d73c879889b45b581f81fa8ede
HsounaHub <hsounaali10@gmail.com>
2023-06-22 16:51:25 +0100

    More Forms

--- React/functional_components/hook_form/src/components/Form.jsx
+++ React/functional_components/hook_form/src/components/Form.jsx
@@ -5,28 +5,74 @@ function Form() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState(""); 
     const [ConfirmPassword, setConfirmPassword] = useState(""); 
-  return (
+    const [errorr,seterrorr]= useState({FirstName:false,LastName:false,email:false,password:false,ConfirmPassword:false});
+    const validateFirstName = (e) =>{
+        setFirstName(e.target.value)
+
+        if(e.target.value.length < 3){
+            seterrorr({FirstName:true});
+        }else{
+            seterrorr({FirstName:false});
+        }}
+    const validatesetLastName = (e) =>{
+            setLastName(e.target.value)
+    
+            if(e.target.value.length < 3){
+                seterrorr({LastName:true});
+            }else{
+                seterrorr({LastName:false});
+            }}
+            const validateemail = (e) =>{
+                setEmail(e.target.value)
+        
+                if(e.target.value.length < 6){
+                    seterrorr({email:true});
+                }else{
+                    seterrorr({email:false});
+                }}
+                const validatepassword = (e) =>{
+                    setPassword(e.target.value)
+            
+                    if(e.target.value!= ConfirmPassword){
+                        seterrorr({password:true});
+                    }else{
+                        seterrorr({password:false});
+                    }}
+                    const validateConfirmPassword = (e) =>{
+                        setConfirmPassword(e.target.value)
+                
+                        if(e.target.value!= password){
+                            seterrorr({ConfirmPassword:true});
+                        }else{
+                            seterrorr({ConfirmPassword:false});
+                        }}
+    return (
     <div>
     <form >
             <div>
                 <label>First Name: </label> 
-                <input type="text" onChange={ (e) => setFirstName(e.target.value) } />
+                <input type="text" onChange={ (e) => validateFirstName(e) } />
+                {errorr.FirstName ?  <p style={{color:"red"}}>Too Short !</p>: ""}
             </div>
             <div>
                 <label>Last Name: </label> 
-                <input type="text" onChange={ (e) => setLastName(e.target.value) } />
+                <input type="text" onChange={ (e) => validatesetLastName(e) } />
+                {errorr.LastName ?  <p style={{color:"red"}}>Too Short !</p>: ""}
             </div>
             <div>
                 <label>Email Address: </label> 
-                <input type="text" onChange={ (e) => setEmail(e.target.value) } />
+                <input type="text" onChange={ (e) => validateemail(e) } />
+                {errorr.email ?  <p style={{color:"red"}}>Too Short !</p>: ""}
             </div>
             <div>
                 <label>Password: </label>
-                <input type="text" onChange={ (e) => setPassword(e.target.value) } />
+                <input type="text" onChange={ (e) => validatepassword(e) } />
+                {errorr.password ?  <p style={{color:"red"}}>password not confirmed !</p>: ""}
             </div>
             <div>
                 <label>Confirm Password: </label>
-                <input type="text" onChange={ (e) => setConfirmPassword(e.target.value) } />
+                <input type="text" onChange={ (e) => validateConfirmPassword(e) } />
+                {errorr.ConfirmPassword ?  <p style={{color:"red"}}>password not confirmed !</p>: ""}
             </div>
             <input type="submit" value="Create User" />
         </form>
@@ -38,5 +84,4 @@ function Form() {
         </div>
   )
 }
-
 export default Form
