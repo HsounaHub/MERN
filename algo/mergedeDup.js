770dc75f42d01961202aec6a5b301a71aa49fc46
HsounaHub <hsounaali10@gmail.com>
2023-06-29 13:20:34 +0100

    Implement Quicksort (Optional)

---
+++ algo/mergedeDup.js
@@ -0,0 +1,11 @@
+function mergeDedup(arr1,arr2){
+    const mergednodup=[]
+    for(var i=0;i<arr1.length;i++){
+        if(arr1[i]=!arr1[i+1]){
+        }
+        for(var j=0;j<arr2.length;j++){
+            if(arr1[i]==arr2[j]){
+            }
+        }
+    }
+}
---
+++ javascript/Fundamentals/quicksort.js
@@ -0,0 +1,40 @@
+function swap(items, leftIndex, rightIndex){
+    var temp = items[leftIndex];
+    items[leftIndex] = items[rightIndex];
+    items[rightIndex] = temp;
+}
+function partition(items, left, right) {
+    var pivot   = items[Math.floor((right + left) / 2)],
+        i       = left, //left pointer
+        j       = right; //right pointer
+    while (i <= j) {
+        while (items[i] < pivot) {
+            i++;
+        }
+        while (items[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            swap(items, i, j); //sawp function
+            i++;
+            j--;
+        }
+    }
+    return i;
+}
+
+function quickSort(items, left, right) {
+    var index;
+    if (items.length > 1) {
+        index = partition(items, left, right); //index returned from partition
+        if (left < index - 1) { //more elements on the left side of the pivot
+            quickSort(items, left, index - 1);
+        }
+        if (index < right) { //more elements on the right side of the pivot
+            quickSort(items, index, right);
+        }
+    }
+    return items;
+}
+const arr1=[1, 4, 2, 7, 6, 3, 8, 20, 9, 15, 12, 10, 30 ]
+console.log(quickSort(arr1,0,arr1.length-1));
