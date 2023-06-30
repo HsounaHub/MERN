ecefe841a77b5aad89c406a6775f0d01ab1ba6cd
HsounaHub <hsounaali10@gmail.com>
2023-06-30 17:56:27 +0100

    Pokemon API

---
+++ React/api/pokemonapi/src/App.js
@@ -0,0 +1,33 @@
+import {useState} from "react";
+
+function App() { 
+  const [pokemon, setpokemon] = useState([]);
+
+  const fetchpokemon = () =>{
+
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=807")
+    .then((serverResponse)=>{
+      return serverResponse.json();
+    })
+    .then((jsonResponse) =>{
+      console.log(jsonResponse.results);
+      setpokemon(jsonResponse.results)
+    })
+    .catch((errObj)=>{console.log("❌❌ Got an Error =>", errObj)})    
+  }
+
+  return (
+    <div className="App">
+    <button onClick={fetchpokemon}>Fetch Pokemon</button>
+   <ul>
+    {pokemon.map((p)=>{
+    return(<li>{p.name}</li>) })
+    }
+    </ul>
+    </div>
+  );
+
+
+}
+
+export default App;
